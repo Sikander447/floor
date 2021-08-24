041ee16c590df6fc9eca90469c2ad302a85ca582
sikander <chdhilloon575@gmail.com>
2021-08-24 17:49:03 +0500

    meets virtual

--- lib/screens/meetsvirtual/meets_virtual.dart
+++ lib/screens/meetsvirtual/meets_virtual.dart
@@ -9,6 +9,44 @@ class MeetsVirtualScreen extends StatefulWidget {
   _MeetsVirtualScreenState createState() => _MeetsVirtualScreenState();
 }
 class _MeetsVirtualScreenState extends State<MeetsVirtualScreen> {
+
+  int counter = 1;
+
+  var currentP;
+  void increment() {
+    // var p = User.userData.restaurantMenu.menu[widget.index].price;
+    if (counter >= 15) {
+    } else {
+      setState(() {
+        counter++;
+
+        // User.userData.restaurantMenu.menu[widget.index].price = p + currentP;
+      });
+    }
+  }
+
+  void decrement() {
+    // var p = User.userData.restaurantMenu.menu[widget.index].price;
+    if (counter <= 1) {
+    } else {
+      setState(() {
+        counter--;
+
+        // User.userData.restaurantMenu.menu[widget.index].price = p - currentP;
+      });
+    }
+  }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      counter;
+      // currentP = User.userData.restaurantMenu.menu[widget.index].price;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
@@ -74,13 +112,122 @@ class _MeetsVirtualScreenState extends State<MeetsVirtualScreen> {
                     SizedBox(
                       height: height * 0.005,
                     ),
-                    RegularText(
-                      text: 'Title',
-                      textOverFlow: TextOverflow.ellipsis,
-                      maximumLine: 1,
-                      size: 17,
-                      fontWeight: FontWeight.w600,
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12,left: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RegularText(
+                            text: 'Title',
+                            textOverFlow: TextOverflow.ellipsis,
+                            maximumLine: 1,
+                            size: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
+                          Container(
+                            height: height * 0.06,
+                            child: TextField(
+                              controller: null,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    // color: Colors.pink[100],
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    // color: Colors.pink[100],
+                                  ),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white,
+                                // contentPadding: EdgeInsets.symmetric(horizontal: 10,vertical: 30),
+                                hintText: 'What do you want to talk about ?',
+                                hintStyle: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  // color: Colors.pink[100],
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: height * 0.02,
+                          ),
+                          RegularText(
+                            text: 'Choose Topics',
+                            textOverFlow: TextOverflow.ellipsis,
+                            maximumLine: 1,
+                            size: 17,
+                            fontWeight: FontWeight.w600,
+                          ),
+
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
+
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              width: width * .33,
+                              height: height * .08,
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius:
+                                  BorderRadius.circular(width * .12),
+                                  border:
+                                  Border.all(color: AppColors.KappBackgroundColor, width: 0.5)),
+                              padding: EdgeInsets.only(left: 2, right: 2),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {
+                                          decrement();
+                                        },
+                                        child: Icon(
+                                          Icons.remove,
+                                          color: Colors.white,
+                                          size: 35,
+                                        ),
+                                      ),
+                                      Text(
+                                        "$counter",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () {
+                                          increment();
+                                        },
+                                        child: Icon(
+                                          Icons.add,
+                                          color: Colors.white,
+                                          size: 35,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
                     ),
+
                   ],
                 ),
               ),
